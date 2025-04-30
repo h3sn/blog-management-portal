cf8f05fd7d0ab7e7a4b88d019b7d84cf8f81f42a
shun nakano <s-nakano@ankh-systems.co.jp>
2025-04-30 16:20:38 +0900

    レイアウトのスタイルを調整

--- src/app/components/layout/DefaultLayout.vue
+++ src/app/components/layout/DefaultLayout.vue
@@ -1,19 +1,27 @@
 <template>
-  <div class="common-layout">
+  <el-container>
+    <el-header><default-header></default-header></el-header>
     <el-container>
-      <el-header><default-header></default-header></el-header>
+      <el-aside width="200px"><default-side-menu></default-side-menu></el-aside>
       <el-container>
-        <el-aside width="200px"><default-side-menu></default-side-menu></el-aside>
-        <el-container>
-          <el-main><RouterView /></el-main>
-          <el-footer><default-footer></default-footer></el-footer>
-        </el-container>
+        <el-main><RouterView /></el-main>
+        <el-footer><default-footer></default-footer></el-footer>
       </el-container>
     </el-container>
-  </div>
+  </el-container>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.el-header {
+  background-color: var(--color-primary);
+}
+.el-aside {
+  border-right: 1px solid var(--color-primary);
+}
+.el-footer {
+  border-top: 1px solid var(--color-primary);
+}
+</style>
 
 <script lang="ts" setup>
 // components
